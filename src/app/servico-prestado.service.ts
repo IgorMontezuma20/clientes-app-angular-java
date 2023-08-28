696e028163aedb9c00b2c2477bb18a5edd3ec4fd
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-28 16:25:47 -0300

    feat: implementando pesquisa de ServicosPrestados

--- src/app/servico-prestado.service.ts
+++ src/app/servico-prestado.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { Observable } from 'rxjs';
 
 import {environment} from '../environments/environment'
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-list/servicoPrestadoBusca';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,16 @@ export class ServicoPrestadoService {
   createService(servicoPrestado: ServicoPrestado):Observable<ServicoPrestado>{
 
     return this.http.post<ServicoPrestado>(this.apiURL, servicoPrestado)
+  }
+
+  searchService(nome: string, mes: number): Observable<ServicoPrestadoBusca[]>{
+
+    const httpParams = new HttpParams()
+    .set("nome", nome)
+    .set("mes", mes.toString());
+
+    const url = this.apiURL + "?" + httpParams.toString()
+    console.log(url)
+    return this.http.get<any>(url)
   }
 }
--- src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
+++ src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicoPrestadoBusca } from './servicoPrestadoBusca';
+import { ServicoPrestadoService } from '../../servico-prestado.service';
 
 @Component({
   selector: 'app-servico-prestado-list',
@@ -10,13 +12,20 @@ export class ServicoPrestadoListComponent implements OnInit {
   nome: string;
   mes: number;
   meses: number[];
+  lista: ServicoPrestadoBusca[];
 
-  ngOnInit(){
+  constructor(private service: ServicoPrestadoService){
     this.meses = [1,2,3,4,5,6,7,8,9,10,11,12]
   }
 
+  ngOnInit(){
+   
+  }
+
   search(){
-    console.log(this.nome, this.mes, this.mes);
+    this.service
+    .searchService(this.nome, this.mes)
+    .subscribe(response => this.lista = response);
   }
 
 }
