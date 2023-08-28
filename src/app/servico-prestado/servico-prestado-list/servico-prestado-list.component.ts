4a843ea86835106b1354009c6fe6e85f432d0120
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-28 15:46:37 -0300

    feat: criando o form de pesquisa para os ServicosPrestados

--- src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
+++ src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servico-prestado-list',
   templateUrl: './servico-prestado-list.component.html',
   styleUrls: ['./servico-prestado-list.component.css']
 })
-export class ServicoPrestadoListComponent {
+export class ServicoPrestadoListComponent implements OnInit {
+
+  nome: string;
+  mes: number;
+  meses: number[];
+
+  ngOnInit(){
+    this.meses = [1,2,3,4,5,6,7,8,9,10,11,12]
+  }
+
+  search(){
+    console.log(this.nome, this.mes, this.mes);
+  }
 
 }
