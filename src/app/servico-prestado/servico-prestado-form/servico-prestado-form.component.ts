dc4f87d94a67c235c847521014b79409a2f5667f
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-28 11:03:50 -0300

    feat: criando o form de ServicosPrestados

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../clientes/Cliente';
+import { ClientesService } from 'src/app/clientes.service';
 
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
   styleUrls: ['./servico-prestado-form.component.css']
 })
-export class ServicoPrestadoFormComponent {
+export class ServicoPrestadoFormComponent implements OnInit {
+
+  clientes: Cliente[] = []
+
+  constructor(
+    private clientesService: ClientesService
+  ){}
+
+  ngOnInit(): void {
+    this.clientesService
+    .getAllClients()
+    .subscribe(response => this.clientes = response)
+  }
+
+  onSubmit(){
+    console.log("Submit")
+  }
 
 }
