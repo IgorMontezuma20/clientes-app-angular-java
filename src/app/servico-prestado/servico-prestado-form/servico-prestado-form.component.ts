232e525edaac6d9c6262b0130bd3f0f54cc51cc0
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-28 11:33:52 -0300

    feat: concluindo o form de ServicosPrestados

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../clientes/Cliente';
 import { ClientesService } from 'src/app/clientes.service';
+import { ServicoPrestado } from '../servicoPrestado';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -10,10 +11,13 @@ import { ClientesService } from 'src/app/clientes.service';
 export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = []
+  servico: ServicoPrestado;
 
   constructor(
     private clientesService: ClientesService
-  ){}
+  ){
+    this.servico = new ServicoPrestado();
+  }
 
   ngOnInit(): void {
     this.clientesService
@@ -22,7 +26,7 @@ export class ServicoPrestadoFormComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log("Submit")
+    console.log(this.servico)
   }
 
 }
