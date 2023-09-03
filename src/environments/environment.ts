fc9f650a348259212665b11bd70c643a33852ff9
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-03 19:32:27 -0300

    ajuste na url da api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiBaseURL: 'clientes-app-production.up.railway.app/',
+    apiBaseURL: 'clientes-app.up.railway.app',
     clientId: 'my-angular-app',
     clientSecret: '@321',
     getTokenUrl: '/oauth/token'
